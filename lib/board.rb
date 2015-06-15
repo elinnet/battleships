57bdae7b6566bca663e1a74cc3dca6ed061c262a
Elin <elinnet@gmail.com>
2015-06-15 17:17:28 +0100

    board.rb add empty ship_array upon initialization

--- lib/board.rb
+++ lib/board.rb
@@ -3,8 +3,19 @@ require_relative '../lib/ship.rb'
 
 class Board 
 
+  attr_reader :ship_array
+
+  def initialize
+    @ship_array = []
+
+  end
+
+
+
+
+
   def place_ship ship
-    
+
   end
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,5 +6,10 @@ describe Board do
     expect(subject).to respond_to(:place_ship).with(1).argument
   end  
 
+  it 'when created, array of ships is empty' do
+    expect(subject.ship_array).to eq []
+  end
+
+
   
 end
